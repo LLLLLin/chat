4e1992a27976710a3fe4bd1c4e8a612a918e0bb7
llllllin <775262285@qq.com>
2019-03-08 08:44:35 +0800

    添加了在离开房间后显示信息

--- chat/server/event.py
+++ chat/server/event.py
@@ -7,7 +7,7 @@ from .. import socketio
 def joined(message):
     print('debug', message)
     room = message.get('msg','')
-
+    session['room'] = room
     join_room(room)
     emit('status',{'msg': session.get('name') + 'join the room.'}, room= room)
 
@@ -15,9 +15,10 @@ def joined(message):
 def text(message):
     room = message.get('room','')
     print('debug',room)
-    emit('message',{'msg': message.get('msg')},room=room)
+    emit('message',{'msg': session.get('name')+':'+message.get('msg')},room=room)
 
 @socketio.on('left', namespace='/chat')
 def left(message):
-    leave_room()
-    emit('status',{'msg' : session.get('name')+ ':' + 'leave the room'})
+    room = session.get('room','')
+    leave_room(room)
+    emit('status',{'msg' : session.get('name')+ ':' + 'leave the room'}, room = room)
